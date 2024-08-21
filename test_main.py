9c2f6dfb0dbc124e78b3d5baa5ae331c5a707514
Ahmed Zidane <ahmedziidanee@gmail.com>
2024-08-21 18:19:55 +0300

    pytest

--- test_main.py
+++ test_main.py
@@ -7,6 +7,8 @@ Date: 21-08-2024
 
 from fastapi.testclient import TestClient
 from main import app
+from unittest.mock import patch
+
 
 client = TestClient(app)
 
@@ -28,26 +30,55 @@ def test_predict_positive():
     """
     response = client.post(
         "/predict/",
-        json=[{
-            "age": 39,
-            "workclass": "State-gov",
-            "fnlwgt": 77516,
-            "education": "Bachelors",
-            "education-num": 13,
-            "marital-status": "Never-married",
-            "occupation": "Adm-clerical",
-            "relationship": "Not-in-family",
-            "race": "White",
-            "sex": "Male",
-            "capital-gain": 2174,
-            "capital-loss": 0,
-            "hours-per-week": 40,
-            "native-country": "United-States",
-        }],
+        json=[
+            {
+                "age": 39,
+                "workclass": "State-gov",
+                "fnlwgt": 77516,
+                "education": "Bachelors",
+                "education-num": 13,
+                "marital-status": "Never-married",
+                "occupation": "Adm-clerical",
+                "relationship": "Not-in-family",
+                "race": "White",
+                "sex": "Male",
+                "capital-gain": 2174,
+                "capital-loss": 0,
+                "hours-per-week": 40,
+                "native-country": "United-States",
+            }
+        ],
     )
     assert response.status_code == 200
     assert response.json() == {"predictions": [">50K"]}
 
+
+def test_predict_positive():
+    with patch("main.encoder", create=True) as mock_encoder:
+        mock_encoder.return_value = "mocked_encoder"
+        response = client.post(
+            "/predict/",
+            json=[
+                {
+                    "age": 39,
+                    "workclass": "State-gov",
+                    "fnlwgt": 77516,
+                    "education": "Bachelors",
+                    "education-num": 13,
+                    "marital-status": "Never-married",
+                    "occupation": "Adm-clerical",
+                    "relationship": "Not-in-family",
+                    "race": "White",
+                    "sex": "Male",
+                    "capital-gain": 2174,
+                    "capital-loss": 0,
+                    "hours-per-week": 40,
+                    "native-country": "United-States",
+                }
+            ],
+        )
+        assert response.status_code == 200
+        assert response.json() == {"predictions": [">50K"]}
 
 def test_predict_negative():
     """
